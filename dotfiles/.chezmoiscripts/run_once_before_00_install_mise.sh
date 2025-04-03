9313e66ba3d19c0d14218e499089513e9bfe08a9
nulldomain <git@nulldoma.in>
2025-04-03 11:58:17 +0000

    feat: initial commit

---
+++ dotfiles/.chezmoiscripts/run_once_before_00_install_mise.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# -e - exit immediately if a command exits with a non-zero status
+# -u - treat unset variables as an error when substituting
+# -o pipefail - return the exit status of the last command in the pipeline that failed
+set -euo pipefail
+
+sh -c "$(curl -fsSL https://mise.run)"
