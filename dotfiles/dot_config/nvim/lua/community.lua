a049afe2218da4ba5b2a53346831ad9a73228bd4
nulldomain <git@nulldoma.in>
2025-04-04 23:49:40 +0000

    feat: .config/nvim

---
+++ dotfiles/dot_config/nvim/lua/community.lua
@@ -0,0 +1,19 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.icon.mini-icons" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+}
